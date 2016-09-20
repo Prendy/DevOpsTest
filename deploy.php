9e3fb0c909b4ac1e5c84452792140d27a32bf551
Andrew Prendergast <andrew.prendergast@skybettingandgaming.com>
2016-09-20 15:18:05 +0100

    added password

--- deploy.php
+++ deploy.php
@@ -1,5 +1,9 @@
 <?php
       echo "Deploying some mental code";
+      $key = $_POST['payload'];
+
+      if($key == "devops") {
       $message=shell_exec("/www/deploy.sh");
       print_r($message);
+    }
 ?>
